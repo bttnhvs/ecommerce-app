6e531ac44241a6f4d104347fd3e1959141b97c14
Bettina <hevesi.bettina@gmail.com>
2025-06-29 18:27:54 +0200

    Refactor Products component to use Reactive Forms for quantity input, implement product fetching and add to cart functionality, and enhance layout with product details.

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,11 +1,61 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit, output, OutputEmitterRef} from '@angular/core';
+import {FormGroup, FormControl, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { Product } from '../../models/product';
+import { ProductService } from '../../service/product.service';
+import {CommonModule} from '@angular/common';
 
 @Component({
-  selector: 'app-products',
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './products.component.html',
+  standalone: true,
   styleUrl: './products.component.scss'
 })
-export class ProductsComponent {
+export class Products implements OnInit {
+  products: Product[] = [];
+  productForms: { [productId: string]: FormGroup } = {};
+  private readonly productStore: ProductService = inject(ProductService);
+  public readonly selectedChanged: OutputEmitterRef<Product> = output();
 
+  ngOnInit(): void {
+    this.productStore.fetchProducts();
+    const data = this.productStore.products();
+    this.products = data;
+    this.products.forEach(product => {
+      this.productForms[product.id] = new FormGroup({
+        quantity: new FormControl(
+          product.minOrderAmount,
+          [
+            Validators.required,
+            Validators.min(product.minOrderAmount),
+            Validators.max(product.availableAmount)
+          ]
+        )
+      });
+    });
+  }
+
+  getForm(productId: string): FormGroup {
+    return this.productForms[productId];
+  }
+
+  addToCart(product: Product): void {
+    const form = this.getForm(product.id);
+    const amount = form.get('quantity')?.value;
+
+    if (form.invalid) {
+      alert('A megadott mennyiség érvénytelen!');
+      return;
+    }
+    this.selectedChanged.emit(product);
+    console.log(product);
+    const success = this.productStore.addToCart(product, amount);
+    console.log(success);
+    if (!success) {
+      alert('Nem lehet ennyit hozzáadni a kosárhoz.');
+    } else {
+      form.get('quantity')?.setValue(product.minOrderAmount);
+    }
+  }
 }
+
+
